cb49231e29dc5c983e85881308db30c9c70b7379
Ruud van Vliet <ruud.van.vliet@trivento.nl>
2022-12-24 17:16:45 +0100

    check_url takes a Vec<url> i.s.o a single url

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use select::document::Document;
 use select::predicate::Name;
 
 fn main() {
-    let debug = false;
-    let base_url = "http://stevensbikeservice.nl";
+    let debug = true;
+    let base_url = "https://stevensbikeservice.nl";
 
     let client = Client::new();
 
@@ -14,10 +14,10 @@ fn main() {
     let mut checked_pages = HashSet::new();
     let mut checked_links = HashSet::new();
     // the check_result is Option<String>: None if ok, the link + error if not ok
-    let mut check_results = HashSet::new();
+    let mut check_results: HashSet<Option<String>> = HashSet::new();
     // initialise the pages that must still be checked with the root of the website
     let mut to_be_checked_pages = HashSet::new();
-    to_be_checked_pages.insert("/".to_string());
+    to_be_checked_pages.insert(base_url.to_string());
 
     while to_be_checked_pages.len() > 0 {
         // pop a random page to be checked
@@ -25,20 +25,20 @@ fn main() {
         to_be_checked_pages.remove(&page_being_checked);
 
         if debug {
-            println!("=============== Checking {page_being_checked} of {} ====================", to_be_checked_pages.len());
+            println!("=============== Checking {page_being_checked}, remaining {} ====================", to_be_checked_pages.len());
             to_be_checked_pages.clone().into_iter().for_each(|s| print!("{s}, "));
             println!();
         }
 
-        let hrefs = crawl(&client, &format!("{base_url}{page_being_checked}"));
+        let hrefs = crawl(&client, &format!("{page_being_checked}"));
 
         // determine the pages we did not yet see
         let new_pages = hrefs
             .clone()
             .into_iter()
-            .map(|href| strip_url(&href, &base_url))
+            .map(|href| format_url(&href, &base_url))
             .filter(|href| (href.starts_with("/") || href.starts_with(base_url))
-                && href != page_being_checked
+                && href != &page_being_checked
                 && !checked_pages.contains(href)
                 && !to_be_checked_pages.contains(href))
             .collect::<HashSet<String>>();
@@ -49,37 +49,37 @@ fn main() {
             println!();
         }
 
-        // check all new pages
-        new_pages
+        // determine the links we did not yet see
+        let new_links = hrefs
             .clone()
             .into_iter()
-            .for_each(|new_page| {
-                check_results.insert(check_url(&client, &format!("{base_url}{new_page}"), debug));
-            });
+            .map(|href| format_url(&href, &base_url))
+            .filter(|href| href.starts_with("http")
+                && !href.starts_with(base_url)
+                && !checked_links.contains(href))
+            .collect::<HashSet<String>>();
 
-        // and insert them into the to_be_checked_pages
+        // concatenate new_pages and new_urls to check them as a batch
+        let new_urls = [&Vec::from_iter(new_pages.clone())[..], &Vec::from_iter(new_links.clone())[..]].concat();
+        print!("new_urls ({}): ", new_urls.len());
+        if new_urls.len() > 0 {
+            for new_url in new_urls.clone() {
+                print!("{new_url}, ");
+            }
+            println!();
+        }
+        for check_result in check_url(&client, new_urls, debug) {
+            check_results.insert(check_result);
+        }
+
+        // insert the new_pages into the to_be_checked_pages
         new_pages
             .into_iter()
             .for_each(|item| {
                 to_be_checked_pages.insert(item);
             });
 
-        // determine the links we did not yet see
-        let new_links = hrefs
-            .into_iter()
-            .map(|href| strip_url(&href, &base_url))
-            .filter(|href| href.starts_with("http") && !checked_links.contains(href))
-            .collect::<HashSet<String>>();
-
-        // check them
-        new_links
-            .clone()
-            .into_iter()
-            .for_each(|new_link| {
-                check_results.insert(check_url(&client, &new_link, debug));
-            });
-
-        // and add them to the checked_links
+        // and insert the new_links into the checked_links
         new_links
             .into_iter()
             .for_each(|item| {
@@ -101,7 +101,7 @@ fn main() {
     }
     let bad_urls = check_results
         .into_iter()
-        .flatten().collect::<Vec<_>>();
+        .flatten().collect::<Vec<String>>();
     let nr_bad_urls = bad_urls.clone().len();
     if nr_bad_urls == 0 {
         println!("--> er zijn GEEN gebroken urls.");
@@ -114,17 +114,16 @@ fn main() {
     }
 
     // strip a trailing '/' and/or the base_url from the url
-    fn strip_url(s: &str, prefix: &str) -> String {
-        let mut chars = s.chars();
-        if s.ends_with("/") {
-            chars.next_back();
+    fn format_url(s: &str, prefix: &str) -> String {
+        let mut tmp = s.to_string();
+        if tmp.ends_with("/") {
+            tmp.pop();
         }
-        if s.starts_with(prefix) {
-            for _i in 0..prefix.len() {
-                chars.next();
-            }
+        if tmp.starts_with("/") {
+            format!("{}{}", prefix, tmp)
+        } else {
+            tmp
         }
-        chars.as_str().to_string()
     }
 
     // fetch all href's from the page, so both pages & links
@@ -139,43 +138,48 @@ fn main() {
         links
     }
 
-    // check url (page or link) for HTTP status code between 200 and 399
-    // return None if ok, Some(link+error) if not ok
-    // TODO accept Vec of urls and check async in parallel
-    fn check_url(client: &Client, url: &str, debug: bool) -> Option<String> {
-        let response =
-            client
-                .get(url)
-                .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:108.0) Gecko/20100101 Firefox/108.0")
-                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
-                .header("Accept-Language", "nl,en-US;q=0.7,en;q=0.3")
-                .header("Accept-Encoding", "gzip, deflate, br")
-                .header("DNT", "1")
-                .header("Connection", "keep-alive")
-                .header("Upgrade-Insecure-Requests", "1")
-                .header("Sec-Fetch-Dest", "document")
-                .header("Sec-Fetch-Mode", "navigate")
-                .header("Sec-Fetch-Site", "none")
-                .header("Sec-Fetch-User", "?1")
-                .header("Pragma", "no-cache")
-                .header("Cache-Control", "no-cache")
-                .send();
-        match response {
-            Ok(res) => {
-                if res.status() >= StatusCode::from_u16(200).unwrap()
-                    && res.status() < StatusCode::from_u16(400).unwrap() {
-                    if debug { println!("{}: success {:?}", url, res.status()); }
-                    None
-                } else {
-                    println!("!!!!! ERROR {}: {:?}", url, res.status());
-                    Some(format!("{link} gave status {:?}", res.status()))
+    // checks Vec<url> (pages or links) for HTTP status code between 200 and 399
+    // returns None if ok, Some(link+error) if not ok
+    // TODO check async in parallel
+    fn check_url(client: &Client, urls: Vec<String>, debug: bool) -> HashSet<Option<String>> {
+        let mut result = HashSet::new();
+
+        for url in urls {
+            let response =
+                client
+                    .get(&url)
+                    .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:108.0) Gecko/20100101 Firefox/108.0")
+                    .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+                    .header("Accept-Language", "nl,en-US;q=0.7,en;q=0.3")
+                    .header("Accept-Encoding", "gzip, deflate, br")
+                    .header("DNT", "1")
+                    .header("Connection", "keep-alive")
+                    .header("Upgrade-Insecure-Requests", "1")
+                    .header("Sec-Fetch-Dest", "document")
+                    .header("Sec-Fetch-Mode", "navigate")
+                    .header("Sec-Fetch-Site", "none")
+                    .header("Sec-Fetch-User", "?1")
+                    .header("Pragma", "no-cache")
+                    .header("Cache-Control", "no-cache")
+                    .send();
+            match response {
+                Ok(res) => {
+                    if res.status() >= StatusCode::from_u16(200).unwrap()
+                        && res.status() < StatusCode::from_u16(400).unwrap() {
+                        if debug { println!("{}: success {:?}", url, res.status()); }
+                        result.insert(None);
+                    } else {
+                        println!("!!!!! ERROR {}: {:?}", url, res.status());
+                        result.insert(Some(format!("{url} gave status {:?}", res.status())));
+                    }
+                }
+                Err(err) => {
+                    println!("!!!!! ERROR: {}: {}", url, err);
+                    result.insert(Some(format!("{url} gave error {:?}", err)));
                 }
             }
-            Err(err) => {
-                println!("!!!!! ERROR: {}: {}", url, err);
-                Some(format!("{link} gave error {:?}", err))
-            }
         }
+        result
     }
 }
 
