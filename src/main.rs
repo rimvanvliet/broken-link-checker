303b3c889b6f2797d15d6a11cadd0ec0f3d3a834
Ruud van Vliet <ruud.van.vliet@trivento.nl>
2022-12-24 14:02:07 +0100

    keep track of pages that still must be checked

--- src/main.rs
+++ src/main.rs
@@ -5,65 +5,130 @@ use select::document::Document;
 use select::predicate::Name;
 
 fn main() {
-    let base_url = "http://vliet.io";
+    let debug = false;
+    let base_url = "http://stevensbikeservice.nl";
 
     let client = Client::new();
-    let mut visited_urls = std::collections::HashSet::new();
-    let mut checked_urls = std::collections::HashSet::new();
-    let mut bad_urls = std::collections::HashSet::new();
 
-    // Crawl the website and get all of the links
-    let links = crawl(&client, base_url);
+    // initialise the progress; a page is an internal link, a link is an external link
+    let mut checked_pages = HashSet::new();
+    let mut checked_links = HashSet::new();
+    // the check_result is Option<String>: None if ok, the link + error if not ok
+    let mut check_results = HashSet::new();
+    // initialise the pages that must still be checked with the root of the website
+    let mut to_be_checked_pages = HashSet::new();
+    to_be_checked_pages.insert("/".to_string());
 
-    // Check each link to see if it is valid
-    for link in links {
-        if link.starts_with('/') {
-            let internal_link = format!("{base_url}{link}");
-            bad_urls.insert(check_link(&client, &internal_link));
-            let link_copy = format!("{link}");
-            println!("checking {link_copy}");
-            visited_urls.insert(link);
-            let sublinks = crawl(&client, &internal_link);
-            for sublink in sublinks {
-                if sublink.starts_with("/") {
-                    if !visited_urls.contains(&sublink) {
-                        println!("     >>> {sublink} is nieuw in {link_copy}");
-                        bad_urls.insert(check_link(&client, &format!("{base_url}{sublink}")));
-                        visited_urls.insert(sublink);
-                    }
-                } else if sublink.starts_with("http") {
-                    if !checked_urls.contains(&sublink) {
-                        checked_urls.insert(sublink.to_string());
-                        bad_urls.insert(check_link(&client, &sublink));
-                    }
-                }
-            }
-        } else if link.starts_with("http") {
-            if !checked_urls.contains(&link) {
-                println!("     >>> {link}  checken.");
-                checked_urls.insert(link.to_string());
-                bad_urls.insert(check_link(&client, &link));
-            }
+    while to_be_checked_pages.len() > 0 {
+        // pop a random page to be checked
+        let page_being_checked = to_be_checked_pages.iter().next().unwrap().clone();
+        to_be_checked_pages.remove(&page_being_checked);
+
+        if debug {
+            println!("=============== Checking {page_being_checked} of {} ====================", to_be_checked_pages.len());
+            to_be_checked_pages.clone().into_iter().for_each(|s| print!("{s}, "));
+            println!();
         }
+
+        let hrefs = crawl(&client, &format!("{base_url}{page_being_checked}"));
+
+        // determine the pages we did not yet see
+        let new_pages = hrefs
+            .clone()
+            .into_iter()
+            .map(|href| strip_url(&href, &base_url))
+            .filter(|href| (href.starts_with("/") || href.starts_with(base_url))
+                && href != page_being_checked
+                && !checked_pages.contains(href)
+                && !to_be_checked_pages.contains(href))
+            .collect::<HashSet<String>>();
+
+        if debug {
+            print!("new pages ({}); ", new_pages.len());
+            new_pages.clone().into_iter().for_each(|s| print!("{s}, "));
+            println!();
+        }
+
+        // check all new pages
+        new_pages
+            .clone()
+            .into_iter()
+            .for_each(|new_page| {
+                check_results.insert(check_url(&client, &format!("{base_url}{new_page}"), debug));
+            });
+
+        // and insert them into the to_be_checked_pages
+        new_pages
+            .into_iter()
+            .for_each(|item| {
+                to_be_checked_pages.insert(item);
+            });
+
+        // determine the links we did not yet see
+        let new_links = hrefs
+            .into_iter()
+            .map(|href| strip_url(&href, &base_url))
+            .filter(|href| href.starts_with("http") && !checked_links.contains(href))
+            .collect::<HashSet<String>>();
+
+        // check them
+        new_links
+            .clone()
+            .into_iter()
+            .for_each(|new_link| {
+                check_results.insert(check_url(&client, &new_link, debug));
+            });
+
+        // and add them to the checked_links
+        new_links
+            .into_iter()
+            .for_each(|item| {
+                checked_links.insert(item);
+            });
+
+        // finally add the page_being_checked to the checked_pages
+        checked_pages.insert(page_being_checked);
     }
-    println!("--> de gevonden interne links zijn ({}): ", visited_urls.len());
-    for int_link in visited_urls {
+
+    // print the results
+    println!("--> de gevonden interne links zijn ({}): ", checked_pages.len());
+    for int_link in checked_pages {
         println!("{int_link}");
     }
-
-    println!("--> de gecheckte externe links zijn ({}): ", checked_urls.len());
-    for checked_url in checked_urls {
+    println!("--> de gecheckte externe links zijn ({}): ", checked_links.len());
+    for checked_url in checked_links {
         println!("{checked_url}");
     }
+    let bad_urls = check_results
+        .into_iter()
+        .flatten().collect::<Vec<_>>();
+    let nr_bad_urls = bad_urls.clone().len();
+    if nr_bad_urls == 0 {
+        println!("--> er zijn GEEN gebroken urls.");
+    } else {
+        println!("--> LET OP: ER ZIJN GEBROKEN URLS ({}):", bad_urls.clone().len());
+        // println!("--> bad urls ({})", &bad_urls.cloned().collect::<Vec<_>>().len());
+        for bad_url in bad_urls {
+            println!("{bad_url}");
+        }
+    }
 
-    println!("--> bad urls ({})",
-             bad_urls
-                 .into_iter()
-                 .flatten()
-                 .collect::<Vec<String>>()
-                 .len());
+    // strip a trailing '/' and/or the base_url from the url
+    fn strip_url(s: &str, prefix: &str) -> String {
+        let mut chars = s.chars();
+        if s.ends_with("/") {
+            chars.next_back();
+        }
+        if s.starts_with(prefix) {
+            for _i in 0..prefix.len() {
+                chars.next();
+            }
+        }
+        chars.as_str().to_string()
+    }
 
-    fn crawl(client: &reqwest::blocking::Client, url: &str) -> HashSet<String> {
+    // fetch all href's from the page, so both pages & links
+    fn crawl(client: &Client, url: &str) -> HashSet<String> {
         let mut links = HashSet::new();
         let body = client.get(url).send().unwrap().text().unwrap();
         let document = Document::from(body.as_str());
@@ -74,10 +139,13 @@ fn main() {
         links
     }
 
-    fn check_link(client: &reqwest::blocking::Client, link: &str) -> Option<String> {
+    // check url (page or link) for HTTP status code between 200 and 399
+    // return None if ok, Some(link+error) if not ok
+    // TODO accept Vec of urls and check async in parallel
+    fn check_url(client: &Client, url: &str, debug: bool) -> Option<String> {
         let response =
             client
-                .get(link)
+                .get(url)
                 .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:108.0) Gecko/20100101 Firefox/108.0")
                 .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
                 .header("Accept-Language", "nl,en-US;q=0.7,en;q=0.3")
@@ -96,15 +164,15 @@ fn main() {
             Ok(res) => {
                 if res.status() >= StatusCode::from_u16(200).unwrap()
                     && res.status() < StatusCode::from_u16(400).unwrap() {
-                    println!("{}: success {:?}", link, res.status());
+                    if debug { println!("{}: success {:?}", url, res.status()); }
                     None
                 } else {
-                    println!("!!!!! ERROR {}: {:?}", link, res.status());
+                    println!("!!!!! ERROR {}: {:?}", url, res.status());
                     Some(format!("{link} gave status {:?}", res.status()))
                 }
             }
             Err(err) => {
-                println!("!!!!! ERROR: {}: {}", link, err);
+                println!("!!!!! ERROR: {}: {}", url, err);
                 Some(format!("{link} gave error {:?}", err))
             }
         }
