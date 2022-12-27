ed0c90415d5d83592d628970e823a332e6bbf97b
Ruud <ruud@bikeservice>
2022-12-27 09:14:27 +0100

    mail subject aangepast

--- hc-run.sh
+++ hc-run.sh
@@ -4,9 +4,16 @@ DATE="$(date +%F)"
 HC_HOME=~/hc/hc
 mkdir -p $HC_HOME/log
 
-printf "To: ruud@vliet.io,stevensbikeservice@gmail.com\nFrom: rimvanvliet@gmail.com\nSubject: HTTP check van $DATE\n\n" > $HC_HOME/log/hc-${DATE}.log
 
-hc >> $HC_HOME/log/hc-${DATE}.log
+hc > $HC_HOME/log/hc-${DATE}.log
 
-ssmtp ruud@vliet.io < $HC_HOME/log/hc-${DATE}.log
-
+if [ $? -eq 0 ]
+  then
+    printf "To: stevensbikeservice@gmail.com\nFrom: rimvanvliet@gmail.com\nSubject: HTTP check van $DATE: GEEN gebroken links\n\n" > $HC_HOME/hc-mail.txt
+    cat $HC_HOME/log/hc-${DATE}.log >> $HC_HOME/hc-mail.txt
+  else
+    printf "To: stevensbikeservice@gmail.com\nFrom: rimvanvliet@gmail.com\nSubject: HTTP check van $DATE: *** ER ZIJN GEBROKEN LINKS ***\n\n" > $HC_HOME/hc-mail.txt
+    cat $HC_HOME/log/hc-${DATE}.log >> $HC_HOME/hc-mail.txt
+fi
+ 
+ssmtp stevensbikeservice@gmail.com < $HC_HOME/hc-mail.txt
