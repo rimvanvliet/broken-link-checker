bcd0b8cba3303d7667efb5e68b1bce7f2da6354e
Ruud van Vliet <ruud.van.vliet@trivento.nl>
2022-12-27 09:28:51 +0100

    send mail only on Monday if no broken links are found

--- hc-run.sh
+++ hc-run.sh
@@ -2,18 +2,19 @@
 
 DATE="$(date +%F)"
 HC_HOME=~/hc/hc
+DOW=$(date +%u)
 mkdir -p $HC_HOME/log
 
+hc >$HC_HOME/log/hc-${DATE}.log
 
-hc > $HC_HOME/log/hc-${DATE}.log
-
-if [ $? -eq 0 ]
-  then
-    printf "To: stevensbikeservice@gmail.com\nFrom: rimvanvliet@gmail.com\nSubject: HTTP check van $DATE: GEEN gebroken links\n\n" > $HC_HOME/hc-mail.txt
-    cat $HC_HOME/log/hc-${DATE}.log >> $HC_HOME/hc-mail.txt
-  else
-    printf "To: stevensbikeservice@gmail.com\nFrom: rimvanvliet@gmail.com\nSubject: HTTP check van $DATE: *** ER ZIJN GEBROKEN LINKS ***\n\n" > $HC_HOME/hc-mail.txt
-    cat $HC_HOME/log/hc-${DATE}.log >> $HC_HOME/hc-mail.txt
+if [ $? -eq 0 ]; then
+  if [ $DOW -eq 1 ]; then
+    printf "To: stevensbikeservice@gmail.com\nFrom: rimvanvliet@gmail.com\nSubject: HTTP check van $DATE: GEEN gebroken links\n\n" >$HC_HOME/hc-mail.txt
+    cat $HC_HOME/log/hc-${DATE}.log >>$HC_HOME/hc-mail.txt
+    ssmtp stevensbikeservice@gmail.com <$HC_HOME/hc-mail.txt
+  fi
+else
+  printf "To: stevensbikeservice@gmail.com\nFrom: rimvanvliet@gmail.com\nSubject: HTTP check van $DATE: *** ER ZIJN GEBROKEN LINKS ***\n\n" >$HC_HOME/hc-mail.txt
+  cat $HC_HOME/log/hc-${DATE}.log >>$HC_HOME/hc-mail.txt
+  ssmtp stevensbikeservice@gmail.com <$HC_HOME/hc-mail.txt
 fi
- 
-ssmtp stevensbikeservice@gmail.com < $HC_HOME/hc-mail.txt
